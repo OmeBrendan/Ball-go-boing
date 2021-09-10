a654410c65fb7e2f73cad8a5cdd145d933a93c4e
Brendan Darwinkel <74767318+OmeBrendan@users.noreply.github.com>
2021-09-10 08:38:43 +0000

    2 balletjes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var x,y,v;
+var x,y,v,z,a,b,j,i;
 
 function setup() {
   createCanvas(500, 400);
@@ -6,18 +6,34 @@ function setup() {
   y = 200;
   v = 5;
   z = 5;
+  a = 30
+  b = 100
+  j = 2
+  i = 7
 }
 
 function draw() {
-  background("255");
+  background("black");
 
-  ellipse(x,y,50,50)
+  square(x,y,50)
+  fill("green")
   x = x + v;
   y = y + z;
 
-if(x <= 0 || x > 500){
+if(x <= 0 || x > 450){
   v = v * -1
  }
-if(y <= 0 || y > 400){
+if(y <= 0 || y > 350){
   z = z * -1}
+
+  square(a,b,50)
+  fill("yellow")
+  a = a + j;
+  b = b + i;
+
+if(a <= 0 || a > 450){
+  j = j * -1
+ }
+if(b <= 0 || b > 350){
+  i = i * -1}
 }
